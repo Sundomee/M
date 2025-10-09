76cc68bd31c8be0d56be9930057fbf74b9df9602
domenico <zdom@outlook.it>
2025-10-09 19:54:53 +0200

    refactor: header globale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DlHeaderComponent } from "./shared/components/header/header.component";
+import { DlSidenavComponent } from './shared/components/sidenav/sidenav.component';
+import { AppTheme } from './shared/components/theme-switcher/theme-switcher.component';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DlHeaderComponent, DlSidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
 
+  public readonly sidenavToggled = signal<boolean>(false);
+  private readonly themeService = inject(ThemeService);
+
   ngOnInit() {
     document.body.classList.add('dark-theme');
+  }
+
+  onBackdropClick() {
+    this.sidenavToggled.set(false);
+  }
+
+  onThemeChanged(event: AppTheme) {
+    this.themeService.changeTheme(event)
   }
 
 }
